1400b14ab779c877056398e41c0652275d0c2dec
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-06-01 21:42:47 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,22 @@
 package Assignment2;
 
 public class BinarySearch {
-
+	public int search(int[] nums, int target) {
+		int low = 0;
+		int high = nums.length - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (target > nums[mid]) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		if (low > high) {
+			return -1;
+		}
+		return -1;
+	}
 }
