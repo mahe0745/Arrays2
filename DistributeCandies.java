4abd3fdaca1e3a5ae59aadc0195239747b0274cf
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-06-01 21:43:26 +0530

    Update DistributeCandies.java

--- DistributeCandies.java
+++ DistributeCandies.java
@@ -1,5 +1,18 @@
 package Assignment2;
 
-public class DistributeCandies {
+import java.util.HashSet;
+import java.util.Set;
 
+public class DistributeCandies {
+	public int distributeCandies(int[] arr) {
+		Set<Integer> set = new HashSet<>();
+		for (int i : arr)
+			set.add(i);
+		int total = set.size();
+		int length = arr.length / 2;
+		if (total <= arr.length / 2)
+			return total;
+		else
+			return length;
+	}
 }
