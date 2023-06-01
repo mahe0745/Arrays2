af57bb2f903a56bcd8d1e4ada754eb195f79433e
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-06-01 21:44:04 +0530

    Update MonotonicArray.java

--- MonotonicArray.java
+++ MonotonicArray.java
@@ -1,5 +1,25 @@
 package Assignment2;
 
 public class MonotonicArray {
-
+	 public boolean isMonotonic(int[] nums) {
+	        int length=nums.length;
+	        if(length==1)return true;
+	        boolean asc= true;
+	        boolean desc=true;;
+	       for(int i=0;i<nums.length-1;i++){
+	            if(nums[i]>nums[i+1]){
+	                asc=false;
+	                break;
+	            };
+	        }
+	        for(int i=0;i<length-1;i++){
+	            if(nums[i]<nums[i+1]){
+	                desc=false;
+	                break;
+	            }
+	        }
+	        
+	        if(asc==true || desc==true)return true;
+	        return false;
+	    }
 }
