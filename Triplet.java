c484d948557728912fd4b3ef5f08fe2e2054ccd9
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-06-01 21:44:36 +0530

    Update Triplet.java

--- Triplet.java
+++ Triplet.java
@@ -1,5 +1,18 @@
 package Assignment2;
 
 public class Triplet {
+	public int maximumProduct(int[] arr) {
+		int n = arr.length;
+		if (n < 3)
+			return -1;
 
+		int max_product = Integer.MIN_VALUE;
+
+		for (int i = 0; i < n - 2; i++)
+			for (int j = i + 1; j < n - 1; j++)
+				for (int k = j + 1; k < n; k++)
+					max_product = Math.max(max_product, arr[i] * arr[j] * arr[k]);
+
+		return max_product;
+	}
 }
