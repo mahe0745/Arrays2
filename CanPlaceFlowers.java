c867510ceeddc841d1dcdbcf5d35fe9720240567
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-06-01 21:43:07 +0530

    Update CanPlaceFlowers.java

--- CanPlaceFlowers.java
+++ CanPlaceFlowers.java
@@ -1,5 +1,28 @@
 package Assignment2;
 
 public class CanPlaceFlowers {
-
+	public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count=0;
+        int length = flowerbed.length;
+        if(length>1){
+            if(flowerbed[0]==0 & flowerbed[1]==0){
+                count++;
+                 flowerbed[0]=1;   
+            }
+            if(flowerbed[length-1]==0 & flowerbed[length-2]==0){
+                flowerbed[length-1]=1;
+                count++;}
+        }
+        if(length==1){
+            if(flowerbed[0]==0)count++;
+        }
+        for( int i=1;i<length-1;i++){
+            if((flowerbed[i]==0)&(flowerbed[i-1]==0)&flowerbed[i+1]==0){
+                count++;
+                flowerbed[i]=1;
+            }
+        }
+        if(count>=n)return true;
+        else return false;
+   }
 }
