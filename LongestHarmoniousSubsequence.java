45f7683d310d8610ee48b426d2181b9e7e5d43d4
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-06-01 21:43:45 +0530

    Update LongestHarmoniousSubsequence.java

--- LongestHarmoniousSubsequence.java
+++ LongestHarmoniousSubsequence.java
@@ -1,5 +1,28 @@
 package Assignment2;
 
+import java.util.Arrays;
+
 public class LongestHarmoniousSubsequence {
+
+	public int findLHS(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		Arrays.sort(nums);
+		int left = 0, right = 1;
+		int ArrayLen = 0;
+		while (right < nums.length) {
+			int diff = nums[right] - nums[left];
+			if (diff == 1) {
+				ArrayLen = Math.max(ArrayLen, right - left + 1);
+			}
+			if (diff <= 1) {
+				right++;
+			} else {
+				left++;
+			}
+		}
+		return ArrayLen;
+	}
 
 }
