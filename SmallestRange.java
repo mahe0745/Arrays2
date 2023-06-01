2754c14ac07cb3971b308818af9c211c9bffce80
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-06-01 21:44:21 +0530

    Update SmallestRange.java

--- SmallestRange.java
+++ SmallestRange.java
@@ -1,5 +1,15 @@
 package Assignment2;
 
-public class SmallestRange {
+import java.util.Arrays;
 
+public class SmallestRange {
+	public int smallestRangeI(int[] arr, int k) {
+		Arrays.sort(arr);
+		int min = arr[0];
+		int max = arr[arr.length - 1];
+		if ((min + k) >= max - k)
+			return 0;
+		else
+			return (max - k) - (min + k);
+	}
 }
